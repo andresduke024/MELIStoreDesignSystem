418f29c970b2fcc6563dfbc9ce23299cc93f0aee
Andres Duque <andresduke024@gmail.com>
2025-04-05 22:33:14 -0500

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version: 6.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "MELIStoreDesignSystem",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "MELIStoreDesignSystem",
+            targets: ["MELIStoreDesignSystem"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "MELIStoreDesignSystem"),
+        .testTarget(
+            name: "MELIStoreDesignSystemTests",
+            dependencies: ["MELIStoreDesignSystem"]
+        ),
+    ]
+)
